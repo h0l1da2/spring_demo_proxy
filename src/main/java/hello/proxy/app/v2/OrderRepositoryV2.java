623ee45068c2fa034074c2fee33c787d61007757
h0l1da2 <holiday.k1@icloud.com>
2025-01-14 20:34:06 +0900

    feat: add version 2 classes
    holiday

---
+++ src/main/java/hello/proxy/app/v2/OrderRepositoryV2.java
@@ -0,0 +1,19 @@
+package hello.proxy.app.v2;
+
+public class OrderRepositoryV2 {
+    public void save(String itemId) {
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("Exception!");
+        }
+
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
